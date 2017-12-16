9e9f0fa45e8e4af11e84318150847d92979b136d
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2017-12-16 15:25:22 +0200

    Initial comment

---
+++ app/src/main/java/com/idoston/coloured_tiles_game/MainActivity.java
@@ -0,0 +1,13 @@
+package com.idoston.coloured_tiles_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
