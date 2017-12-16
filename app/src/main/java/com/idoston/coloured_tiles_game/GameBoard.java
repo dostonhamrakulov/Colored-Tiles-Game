86a6fe4cf0040af12971daca18f639f4d737535a
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2017-12-16 22:04:48 +0200

    GameBoard has been created and moves well

---
+++ app/src/main/java/com/idoston/coloured_tiles_game/GameBoard.java
@@ -0,0 +1,13 @@
+package com.idoston.coloured_tiles_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_board2);
+    }
+}
--- app/src/main/java/com/idoston/coloured_tiles_game/MainActivity.java
+++ app/src/main/java/com/idoston/coloured_tiles_game/MainActivity.java
@@ -1,7 +1,9 @@
 package com.idoston.coloured_tiles_game;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void MoveBoard(View view){
+        startActivity(new Intent(MainActivity.this, GameBoard.class));
     }
 }
